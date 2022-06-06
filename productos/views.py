acca8ed88ecafe46e07d37c3b5830cbf0e30b103
Chris Mohr <cjerezmohr@outlook.com>
2022-06-05 23:20:34 -0300

    se muestra en pantalla

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from productos.models import *
 
 # Create your views here.
 def market(request):
-    context = {}
+    equipo_nuevo = Desktop_notebook.objects.all()
+    externo_nuevo = Periferico.objects.all()
+    monitor_nuevo = Monitor.objects.all()
+    context = {'equipo_nuevo':equipo_nuevo,'externo_nuevo':externo_nuevo,'monitor_nuevo':monitor_nuevo}
     return render(request, 'store.html', context=context)
